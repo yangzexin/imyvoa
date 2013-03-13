d48ab75f3a944a6438682eb47128a2f95e50f7b0
yangzexin <yangzexin@yangzexintekiMac-mini.local>
2013-03-13 16:11:37 +0800

    c

---
+++ imyvoa/UIPerfers.h
@@ -0,0 +1,13 @@
+//
+//  UIPerfers.h
+//  imyvoa
+//
+//  Created by yangzexin on 13-3-13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIPerfers : NSObject
+
+@end
---
+++ imyvoa/UIPerfers.m
@@ -0,0 +1,13 @@
+//
+//  UIPerfers.m
+//  imyvoa
+//
+//  Created by yangzexin on 13-3-13.
+//
+//
+
+#import "UIPerfers.h"
+
+@implementation UIPerfers
+
+@end
