df640e1b7eb2cfb7957a9871f5e20081766a1f89
yangzexin <yangzexin@yangzexintekiMac-mini.local>
2013-04-26 16:23:36 +0800

    C

---
+++ TargetForTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TargetForTest
+//
+//  Created by yangzexin on 13-4-26.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TargetForTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TargetForTest
+//
+//  Created by yangzexin on 13-4-26.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
