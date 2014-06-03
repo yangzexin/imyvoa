a825a57e5ac23e87b69642fa78f47f600c45fc08
yangzexin <yangzexin2011@gmail.com>
2014-06-03 22:57:34 +0800

    c

--- imyvoa/BaseViewController.m
+++ imyvoa/BaseViewController.m
@@ -70,16 +70,20 @@
 {
     [super loadView];
     
+    BOOL ios7 = [[UIDevice currentDevice].systemVersion floatValue] >= 7.0f;
+    
     self.titleLabel = [[[UILabel alloc] init] autorelease];
     self.titleLabel.textAlignment = UITextAlignmentCenter;
-    self.titleLabel.textColor = [UIColor whiteColor];
+    self.titleLabel.textColor = !ios7 ? [UIColor whiteColor] : [UIColor blackColor];
     self.titleLabel.backgroundColor = [UIColor clearColor];
     self.titleLabel.font = [UIFont boldSystemFontOfSize:20.0f];
     self.titleLabel.text = _customTitle;
-    self.titleLabel.layer.shadowRadius = 0.5f;
-    self.titleLabel.layer.shadowOpacity = 1.0f;
-    self.titleLabel.layer.shadowOffset = CGSizeMake(0, -0.5f);
-    self.titleLabel.layer.shadowColor = [UIColor blackColor].CGColor;
+    if (!ios7) {
+        self.titleLabel.layer.shadowRadius = 0.5f;
+        self.titleLabel.layer.shadowOpacity = 1.0f;
+        self.titleLabel.layer.shadowOffset = CGSizeMake(0, -0.5f);
+        self.titleLabel.layer.shadowColor = [UIColor blackColor].CGColor;
+    }
     self.titleLabel.adjustsFontSizeToFitWidth = YES;
     self.titleLabel.frame = CGRectMake(0, 0, [_customTitle sizeWithFont:self.titleLabel.font].width, 
                                        self.titleLabel.font.lineHeight);
